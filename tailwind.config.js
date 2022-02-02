e1d5698f1879e5a0ba3a919eb2a890f55b6521c9
Harold Pineda <hrpinedar91@gmail.com>
2022-02-02 15:04:08 -0500

    confiuracion backgroung img tailwindconfig

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {         
+        'sanFrancisco': "url('../img/san-fracisco.jpg')",  
+			  'yosemite': "url('../img/yosemite.jpg')",
+			  'seattle': "url('../img/seattle.jpg')",
+			  'new-york': "url('../img/new-york.jpg')",
+			  'norway': "url('../img/norway.jpg')",
+			  // 'sydney': "url('../img/sydney.jpg')",
+			  // 'sanFranciscoDesktop': "url('../img/sanFranciscoDesktop.jpg')",
+			  // 'LA': "url('../img/LA.jpg')",
+			  // 'miami': "url('../img/miami.jpg')",
+			  // 'switzerland': "url('../img/switzerland.jpg')",
+			  // 'bali': "url('../img/bali.jpg')",
+			  // 'norway': "url('../img/norway.jpg')",
+			  // 'chicago': "url('../img/chicago.jpg')",
+			  // 'europe': "url('../img/europe.jpg')",
+			  // 'iceland': "url('../img/iceland.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
