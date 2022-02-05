8d6e612f8a4454601fea28710d15897c2ea212af
Harold Pineda <hrpinedar91@gmail.com>
2022-02-05 12:06:07 -0500

    animaciones

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,40 @@
 module.exports = {
-  content: ["./public/*.{html,js}"],
-  theme: {
-    extend: {
-      backgroundImage: {         
-        'sanFrancisco': "url('../img/SanFrancisco.png')",  
-		'yosemite': "url('../img/yosemite.jpg')",
-		'seattle': "url('../img/seattle.jpg')",
-		'new-york': "url('../img/new-york.jpg')",
-		'norway': "url('../img/norway.jpg')",		
-		'switzerland': "url('../img/switzerland.jpg')",	 
-      },
-	  backgroundColor: theme => ({
-		  ...theme('colors'),
-		  'primary': '#CC2D4A',
-		  'secondary': '#8FA206',
-		  'terciary': '#61AEC9',
+	content: ["./public/*.{html,js}"],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'sanFrancisco': "url('../img/SanFrancisco.png')",
+				'yosemite': "url('../img/yosemite.jpg')",
+				'seattle': "url('../img/seattle.jpg')",
+				'new-york': "url('../img/new-york.jpg')",
+				'norway': "url('../img/norway.jpg')",
+				'switzerland': "url('../img/switzerland.jpg')",
+			},
+			backgroundColor: theme => ({
+				...theme('colors'),
+				'primary': '#CC2D4A',
+				'secondary': '#8FA206',
+				'terciary': '#61AEC9',
 
-	  }),
-	  textColor: theme => ({
-		  ...theme('colors'),
-		'primary': '#CC2D4A',
-		'secondary': '#8FA206',
-		'terciary': '#61AEC9',
-	  }),
-	//   borderRadius: {
-	// 	'md': '0.434rem'
-	//   },
-	  fontFamily: {
-		'Monserrat': ['Montserrat', 'sans-serif']
-	  },
-    },
-  },
-  plugins: [],
+			}),
+			textColor: theme => ({
+				...theme('colors'),
+				'primary': '#CC2D4A',
+				'secondary': '#8FA206',
+				'terciary': '#61AEC9',
+			}),
+			fontFamily: {
+				'Monserrat': ['Montserrat', 'sans-serif']
+			},
+			ringColor: {
+				'primary': '#CC2D4A',
+				'secondary': '#8FA206',
+				'tertiary': '#61AEC9',
+			}
+		},
+		variants: {
+			with: ["responsive", "hover", "focus"]
+		},
+	},
+	plugins: [],
 }
